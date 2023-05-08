2ef39bff2cdc3e40d93739324e3835587156c432
ramalazzaro <67916718+ramalazzaro@users.noreply.github.com>
2023-05-08 20:50:19 -0300

    Agrega clase texto (solo con sus atributos)

--- src/PerfilInstagram.java
+++ src/PerfilInstagram.java
@@ -4,7 +4,6 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class PerfilInstagram {
-    //ver como hacer esto de forma TREESET, (ejemplo concesionaria en teoria)
     private SortedSet<Publicacion> listaPublicaciones = new TreeSet<>();
     public void addPublicacion(Publicacion nuevaPublicacion) {
         listaPublicaciones.add(nuevaPublicacion);
---
+++ src/Texto.java
@@ -0,0 +1,9 @@
+public class Texto extends Publicacion {
+    private int cantChar;
+    private string fuente;
+    private int tamaño;
+    private string cuerpo; 
+
+    
+
+}
