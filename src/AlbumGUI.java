129d652726080e5f90f3ad023541e4d7aa733c7e
Franco Benettini <franbenettini@gmail.com>
2023-06-14 00:23:59 -0300

    album

--- src/AlbumGUI.java
+++ src/AlbumGUI.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class AlbumGUI extends JFrame {
@@ -9,113 +8,17 @@ public class AlbumGUI extends JFrame {
     public AlbumGUI(PerfilInstagram perfil) {
         createUI(perfil);
         displayAlbum();
-
-        JList<Album> albumList = new JList<>(perfil.getListaAlbumes().toArray(new Album[0]));
-        JScrollPane albumListScrollPane = new JScrollPane(albumList);
-        albumPanel.add(albumListScrollPane); // Assuming albumPanel is the correct place for it
-
-        albumList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
-        // Add the list selection listener to the album list
-        albumList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        albumList.addListSelectionListener(e -> {
-            if (!e.getValueIsAdjusting()) { // Ignore extra events
-                Album selectedAlbum = albumList.getSelectedValue();
-                if (selectedAlbum != null) {
-                    addPublicationsToAlbum(selectedAlbum, perfil); // passing both arguments here
-                }
-            }
-        });
-    }
-
-    public void createAlbum(PerfilInstagram perfilInstagram) {
-
-        // Create a dialog to enter the album name and select the publications
-        JDialog dialog = new JDialog(this, "Crear Álbum", true);
-        dialog.setSize(400, 300);
-        dialog.setLayout(new BorderLayout());
-
-        // Create a text field for entering the album name
-        JTextField albumNameField = new JTextField();
-        dialog.add(albumNameField, BorderLayout.NORTH);
-
-        // Create a list to display the available publications
-        JList<Publicacion> publicationList = new JList<>(
-                perfilInstagram.getListaPublicaciones().toArray(new Publicacion[0]));
-        publicationList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-        JScrollPane scrollPane = new JScrollPane(publicationList);
-        scrollPane.setPreferredSize(new Dimension(350, 200)); // Set a preferred size for the scroll pane
-        dialog.add(scrollPane, BorderLayout.CENTER);
-
-        // Create a button to save the selected publications and close the dialog
-        JButton saveButton = new JButton("Guardar");
-        saveButton.addActionListener(e -> {
-            String albumName = albumNameField.getText().trim(); // Trim leading and trailing spaces
-            if (albumName.isEmpty()) {
-                JOptionPane.showMessageDialog(dialog, "Debe ingresar un nombre para el álbum",
-                        "Error", JOptionPane.ERROR_MESSAGE);
-            } else {
-
-                List<Publicacion> selectedPublications = publicationList.getSelectedValuesList();
-                Album album = new Album(albumName);
-                for (Publicacion publicacion : selectedPublications) {
-                    album.addPublicacion(publicacion);
-                }
-                updateAlbumList(perfilInstagram);
-                perfilInstagram.addAlbum(album);
-                updateAlbumList(perfilInstagram); // Update the album list after saving
-                dialog.dispose();
-            }
-        });
-        dialog.add(saveButton, BorderLayout.SOUTH);
-
-        dialog.setVisible(true);
-    }
-
-    public void addPublicationsToAlbum(Album album, PerfilInstagram perfilInstagram) {
-
-        JDialog dialog = new JDialog(this, "Agregar Publicaciones", true);
-        dialog.setSize(400, 300);
-        dialog.setLayout(new BorderLayout());
-
-        List<Publicacion> availablePublications = new ArrayList<>(perfilInstagram.getListaPublicaciones());
-        availablePublications.removeAll(album.getPublicaciones());
-
-        JList<Publicacion> publicationList = new JList<>(availablePublications.toArray(new Publicacion[0]));
-        publicationList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-        JScrollPane scrollPane = new JScrollPane(publicationList);
-        dialog.add(scrollPane, BorderLayout.CENTER);
-
-        JButton saveButton = new JButton("Guardar");
-        saveButton.addActionListener(e -> {
-            List<Publicacion> selectedPublications = publicationList.getSelectedValuesList();
-            for (Publicacion publicacion : selectedPublications) {
-                album.addPublicacion(publicacion);
-            }
-            updateAlbumList(perfilInstagram); // Update the album list after saving
-            dialog.dispose();
-        });
-        dialog.add(saveButton, BorderLayout.SOUTH);
-
-        dialog.setVisible(true);
+        updateAlbumList(perfil);
     }
 
     private void createUI(PerfilInstagram perfilInstagram) {
-        setTitle("Álbum");
+        setTitle("Lista de Álbumes");
         setSize(800, 600);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
 
         albumPanel = new JPanel();
         albumPanel.setLayout(new GridLayout(0, 1, 10, 10));
-
-        updateAlbumList(perfilInstagram);
-
-        for (Publicacion publicacion : perfilInstagram.getListaPublicaciones()) {
-            JLabel publicacionLabel = new JLabel(publicacion.toString());
-            publicacionLabel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
-            albumPanel.add(publicacionLabel);
-        }
 
         JScrollPane scrollPane = new JScrollPane(albumPanel);
         scrollPane.setPreferredSize(new Dimension(780, 560));
@@ -127,74 +30,37 @@ public class AlbumGUI extends JFrame {
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         buttonPanel.add(createAlbumButton);
         getContentPane().add(buttonPanel, BorderLayout.SOUTH);
-    }
 
-    /*
-     * public void updateAlbumList(PerfilInstagram perfilInstagram) {
-     * albumPanel.removeAll(); // Clear the existing albums
-     * 
-     * for (Album album : perfilInstagram.getListaAlbumes()) {
-     * JLabel albumLabel = new JLabel(
-     * album.toString());
-     * albumLabel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
-     * 
-     * JButton addPublicationsButton = new JButton("Agregar Publicaciones");
-     * addPublicationsButton.addActionListener(e -> addPublicationsToAlbum(album,
-     * perfilInstagram));
-     * 
-     * 
-     * 
-     * JButton deleteButton = new JButton("Eliminar Álbum");
-     * deleteButton.addActionListener(e -> {
-     * perfilInstagram.removeAlbum(album);
-     * updateAlbumList(perfilInstagram);
-     * });
-     * 
-     * JPanel albumEntryPanel = new JPanel(new BorderLayout());
-     * albumEntryPanel.add(albumLabel, BorderLayout.CENTER);
-     * albumEntryPanel.add(addPublicationsButton, BorderLayout.WEST);
-     * albumEntryPanel.add(deleteButton, BorderLayout.EAST);
-     * 
-     * albumPanel.add(albumEntryPanel);
-     * }
-     * 
-     * albumPanel.revalidate(); // Update the albumPanel
-     * albumPanel.repaint();
-     * }
-     */
+    }
 
     public void updateAlbumList(PerfilInstagram perfilInstagram) {
         albumPanel.removeAll(); // Clear the existing albums
 
+        int numRows = perfilInstagram.getListaAlbumes().size();
+        albumPanel.setLayout(new GridLayout(numRows, 2, 10, 10));
+
         for (Album album : perfilInstagram.getListaAlbumes()) {
-            JLabel albumLabel = new JLabel(album.toString());
-            albumLabel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+            JButton albumButton = new JButton(album.toString());
+            albumButton.addActionListener(e -> new IndividualAlbumGUI(album, perfilInstagram));
 
-            JButton addPublicationsButton = new JButton("Agregar Publicaciones");
-            addPublicationsButton.addActionListener(e -> addPublicationsToAlbum(album, perfilInstagram));
-
-            JButton createSubAlbumButton = new JButton("Crear Sub-Álbum");
-            createSubAlbumButton.addActionListener(e -> createSubAlbum(album, perfilInstagram));
-
-            JButton deleteButton = new JButton("Eliminar Álbum");
-            deleteButton.addActionListener(e -> {
+            // Create the album button and delete button
+            JButton deleteAlbumButton = new JButton("Eliminar Álbum");
+            deleteAlbumButton.addActionListener(e -> {
                 perfilInstagram.removeAlbum(album);
                 updateAlbumList(perfilInstagram);
             });
 
-            JPanel albumEntryPanel = new JPanel(new BorderLayout());
-            albumEntryPanel.add(albumLabel, BorderLayout.NORTH);
-            albumEntryPanel.add(addPublicationsButton, BorderLayout.WEST);
-            albumEntryPanel.add(createSubAlbumButton, BorderLayout.CENTER);
-            albumEntryPanel.add(deleteButton, BorderLayout.EAST);
+            // Create the publications panel
+            JPanel publicationsPanel = new JPanel();
+            publicationsPanel.setLayout(new GridLayout(album.getPublicaciones().size(), 1, 10, 10));
 
-            // For each publication in the album, add a delete button
-            JPanel publicationsPanel = new JPanel(new GridLayout(0, 1));
+            // Add the publication buttons
             for (Publicacion publicacion : album.getPublicaciones()) {
                 JPanel publicationEntryPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
                 JLabel publicationLabel = new JLabel(
                         publicacion.getClass().getSimpleName() + "\t" + publicacion.getNombre());
-                JButton deletePublicationButton = new JButton("Eliminar");
+                JButton deletePublicationButton = new JButton("-");
+                deletePublicationButton.setPreferredSize(new Dimension(20, 20));
                 deletePublicationButton.addActionListener(e -> {
                     album.deletePublicacion(publicacion);
                     updateAlbumList(perfilInstagram);
@@ -204,51 +70,47 @@ public class AlbumGUI extends JFrame {
                 publicationsPanel.add(publicationEntryPanel);
             }
 
-            albumEntryPanel.add(publicationsPanel, BorderLayout.SOUTH);
-            albumPanel.add(albumEntryPanel);
-
-            addSubAlbumsToPanel(album, albumEntryPanel, perfilInstagram);
-
+            // Add the components to the album panel
+            albumPanel.add(albumButton);
+            albumPanel.add(deleteAlbumButton);
+            albumPanel.add(publicationsPanel);
         }
 
         albumPanel.revalidate(); // Update the albumPanel
         albumPanel.repaint();
     }
 
-    public void createSubAlbum(Album parentAlbum, PerfilInstagram perfilInstagram) {
-        // Create a dialog to enter the sub-album name and select the publications
-        JDialog dialog = new JDialog(this, "Crear Sub-Álbum", true);
+    public void createAlbum(PerfilInstagram perfilInstagram) {
+        JDialog dialog = new JDialog(this, "Crear Álbum", true);
         dialog.setSize(400, 300);
         dialog.setLayout(new BorderLayout());
 
-        // Create a text field for entering the sub-album name
         JTextField albumNameField = new JTextField();
         dialog.add(albumNameField, BorderLayout.NORTH);
 
-        // Create a list to display the available publications
-        JList<Publicacion> publicationList = new JList<>(
-                perfilInstagram.getListaPublicaciones().toArray(new Publicacion[0]));
+        JList publicationList = new JList(perfilInstagram.getListaPublicaciones().toArray(new Publicacion[0]));
         publicationList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         JScrollPane scrollPane = new JScrollPane(publicationList);
-        scrollPane.setPreferredSize(new Dimension(350, 200)); // Set a preferred size for the scroll pane
         dialog.add(scrollPane, BorderLayout.CENTER);
 
-        // Create a button to save the selected publications and close the dialog
         JButton saveButton = new JButton("Guardar");
         saveButton.addActionListener(e -> {
-            String albumName = albumNameField.getText().trim(); // Trim leading and trailing spaces
+            String albumName = albumNameField.getText().trim();
             if (albumName.isEmpty()) {
-                JOptionPane.showMessageDialog(dialog, "Debe ingresar un nombre para el sub-álbum",
+                JOptionPane.showMessageDialog(dialog, "Debe ingresar un nombre para el álbum",
                         "Error", JOptionPane.ERROR_MESSAGE);
             } else {
-                List<Publicacion> selectedPublications = publicationList.getSelectedValuesList();
                 Album album = new Album(albumName);
-                for (Publicacion publicacion : selectedPublications) {
-                    album.addPublicacion(publicacion);
+                Object[] selectedObjects = publicationList.getSelectedValues();
+                for (Object object : selectedObjects) {
+                    if (object instanceof Publicacion) {
+                        album.addPublicacion((Publicacion) object);
+                    }
                 }
-                parentAlbum.addSubAlbum(album);
-                updateAlbumList(perfilInstagram); // Update the album list after saving
+                perfilInstagram.addAlbum(album);
+                // Since you have created a new album, you should refresh your album's UI here.
                 dialog.dispose();
+                refreshGUI(perfilInstagram);
             }
         });
         dialog.add(saveButton, BorderLayout.SOUTH);
@@ -256,71 +118,27 @@ public class AlbumGUI extends JFrame {
         dialog.setVisible(true);
     }
 
-    public void addSubAlbumsToPanel(Album album, JPanel parentPanel, PerfilInstagram perfilInstagram) {
-        for (Album subAlbum : album.getSubAlbumes()) {
-            JLabel subAlbumLabel = new JLabel(subAlbum.toString());
+    public void refreshGUI(PerfilInstagram perfilInstagram) {
+        albumPanel.removeAll(); // Clear the existing albums
 
-            subAlbumLabel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+        for (Album album : perfilInstagram.getListaAlbumes()) {
+            JButton albumButton = new JButton(album.toString());
+            albumButton.addActionListener(e -> new IndividualAlbumGUI(album, perfilInstagram));
 
-            subAlbumLabel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
-
-            JButton addPublicationsButton = new JButton("Agregar Publicaciones");
-            addPublicationsButton.addActionListener(e -> addPublicationsToAlbum(subAlbum, perfilInstagram));
-
-            JButton createSubAlbumButton = new JButton("Crear Sub-Álbum");
-            createSubAlbumButton.addActionListener(e -> createSubAlbum(subAlbum, perfilInstagram));
-
-            JPanel subAlbumEntryPanel = new JPanel(new BorderLayout());
-
-            JButton deleteButton = new JButton("Eliminar Sub-Álbum");
+            JButton deleteButton = new JButton("Eliminar Álbum");
             deleteButton.addActionListener(e -> {
-                SwingUtilities.invokeLater(() -> {
-                    album.removeSubAlbum(subAlbum);
-                    parentPanel.remove(subAlbumEntryPanel);
-                    parentPanel.revalidate();
-                    parentPanel.repaint();
-                    updateAlbumList(perfilInstagram); // Update the album list after removing subalbum
-                });
+                perfilInstagram.removeAlbum(album);
+                refreshGUI(perfilInstagram);
             });
 
-            subAlbumEntryPanel.add(subAlbumLabel, BorderLayout.NORTH);
-            subAlbumEntryPanel.add(addPublicationsButton, BorderLayout.WEST);
-            subAlbumEntryPanel.add(createSubAlbumButton, BorderLayout.CENTER);
-            subAlbumEntryPanel.add(deleteButton, BorderLayout.EAST);
-
-            JPanel publicationsPanel = new JPanel(new GridLayout(0, 1));
-            for (Publicacion publicacion : subAlbum.getPublicaciones()) {
-                JPanel publicationEntryPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-                JLabel publicationLabel = new JLabel(
-                        publicacion.getClass().getSimpleName() + "\t" + publicacion.getNombre());
-                JButton deletePublicationButton = new JButton("Eliminar");
-                deletePublicationButton.addActionListener(e -> {
-                    SwingUtilities.invokeLater(() -> {
-                        subAlbum.deletePublicacion(publicacion);
-                        publicationsPanel.remove(publicationEntryPanel);
-                        publicationsPanel.revalidate();
-                        publicationsPanel.repaint();
-                    });
-                });
-                publicationEntryPanel.add(publicationLabel);
-                publicationEntryPanel.add(deletePublicationButton);
-                publicationsPanel.add(publicationEntryPanel);
-            }
-
-            subAlbumEntryPanel.add(publicationsPanel, BorderLayout.SOUTH);
-            parentPanel.add(subAlbumEntryPanel);
-
-            JPanel subPanel = new JPanel(new GridLayout(0, 1));
-            subAlbumEntryPanel.add(subPanel, BorderLayout.SOUTH);
-            // Recursive call to handle nested sub-albums
-            addSubAlbumsToPanel(subAlbum, subPanel, perfilInstagram);
-
+            JPanel albumEntryPanel = new JPanel(new FlowLayout());
+            albumEntryPanel.add(albumButton);
+            albumEntryPanel.add(deleteButton);
+            albumPanel.add(albumEntryPanel);
         }
-    }
 
-    public void removeAlbum(Album album, PerfilInstagram perfilInstagram) {
-        perfilInstagram.removeAlbum(album);
-
+        albumPanel.revalidate(); // Update the albumPanel
+        albumPanel.repaint();
     }
 
     private void displayAlbum() {
---
+++ src/IndividualAlbumGUI.java
@@ -0,0 +1,172 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IndividualAlbumGUI extends JFrame {
+    private Album album;
+    private PerfilInstagram perfilInstagram;
+    private JPanel albumPanel;
+
+    public IndividualAlbumGUI(Album album, PerfilInstagram perfilInstagram) {
+        albumPanel = new JPanel();
+        this.album = album;
+        this.perfilInstagram = perfilInstagram;
+
+        setTitle(album.getNombre());
+        setSize(400, 300);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JButton addPublicationButton = new JButton("Agregar Publicaciones");
+        addPublicationButton.addActionListener(e -> addPublicationsToAlbum(album, perfilInstagram));
+
+        JButton createSubAlbumButton = new JButton("Crear Sub-Álbum");
+        createSubAlbumButton.addActionListener(e -> createSubAlbum(album, perfilInstagram));
+
+        JPanel panel = new JPanel();
+        panel.add(addPublicationButton);
+        panel.add(createSubAlbumButton);
+
+        getContentPane().add(panel);
+
+        setVisible(true);
+    }
+
+    public void addPublicationsToAlbum(Album album, PerfilInstagram perfilInstagram) {
+        JDialog dialog = new JDialog(this, "Agregar Publicaciones", true);
+        dialog.setSize(400, 300);
+        dialog.setLayout(new BorderLayout());
+
+        List<Publicacion> availablePublications = new ArrayList<>(perfilInstagram.getListaPublicaciones());
+        availablePublications.removeAll(album.getPublicaciones());
+
+        JList publicationList = new JList(availablePublications.toArray(new Publicacion[0]));
+        publicationList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        JScrollPane scrollPane = new JScrollPane(publicationList);
+        dialog.add(scrollPane, BorderLayout.CENTER);
+
+        JButton saveButton = new JButton("Guardar");
+        saveButton.addActionListener(e -> {
+            Object[] selectedObjects = publicationList.getSelectedValues();
+            for (Object object : selectedObjects) {
+                if (object instanceof Publicacion) {
+                    album.addPublicacion((Publicacion) object);
+                }
+            }
+            // Since you have added publications to album, you should refresh your album's
+            // UI here.
+            dialog.dispose();
+            refreshGUI(perfilInstagram);
+        });
+        dialog.add(saveButton, BorderLayout.SOUTH);
+
+        dialog.setVisible(true);
+    }
+
+    public void removePublicacion(Publicacion publicacion) {
+        album.deletePublicacion(publicacion);
+    }
+
+    public void refreshGUI(PerfilInstagram perfilInstagram) {
+        albumPanel.removeAll(); // Remove old components
+        // Rebuild the albumPanel
+        for (Publicacion publicacion : album.getPublicaciones()) {
+            JLabel publicationLabel = new JLabel(publicacion.toString());
+            publicationLabel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+
+            JButton deleteButton = new JButton("Eliminar Publicacion");
+            deleteButton.addActionListener(e -> {
+                album.deletePublicacion(publicacion);
+                refreshGUI(perfilInstagram);
+            });
+
+            JPanel publicationEntryPanel = new JPanel(new BorderLayout());
+            publicationEntryPanel.add(publicationLabel, BorderLayout.CENTER);
+            publicationEntryPanel.add(deleteButton, BorderLayout.EAST);
+
+            albumPanel.add(publicationEntryPanel);
+        }
+
+        this.getContentPane().revalidate(); // Inform that contents have changed
+        this.getContentPane().repaint(); // Redraw contents
+    }
+
+    public void createSubAlbum(Album parentAlbum, PerfilInstagram perfilInstagram) {
+        // Create a dialog to enter the sub-album name and select the publications
+        JDialog dialog = new JDialog(this, "Crear Sub-Álbum", true);
+        dialog.setSize(400, 300);
+        dialog.setLayout(new BorderLayout());
+
+        // Create a text field for entering the sub-album name
+        JTextField albumNameField = new JTextField();
+        dialog.add(albumNameField, BorderLayout.NORTH);
+
+        // Create a list to display the available publications
+        JList<Publicacion> publicationList = new JList<>(
+                perfilInstagram.getListaPublicaciones().toArray(new Publicacion[0]));
+        publicationList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        JScrollPane scrollPane = new JScrollPane(publicationList);
+        scrollPane.setPreferredSize(new Dimension(350, 200)); // Set a preferred size for the scroll pane
+        dialog.add(scrollPane, BorderLayout.CENTER);
+
+        // Create a button to save the selected publications and close the dialog
+        JButton saveButton = new JButton("Guardar");
+        saveButton.addActionListener(e -> {
+            String albumName = albumNameField.getText().trim(); // Trim leading and trailing spaces
+            if (albumName.isEmpty()) {
+                JOptionPane.showMessageDialog(dialog, "Debe ingresar un nombre para el sub-álbum",
+                        "Error", JOptionPane.ERROR_MESSAGE);
+            } else {
+                List<Publicacion> selectedPublications = publicationList.getSelectedValuesList();
+                Album album = new Album(albumName);
+                for (Publicacion publicacion : selectedPublications) {
+                    album.addPublicacion(publicacion);
+                }
+                parentAlbum.addSubAlbum(album);
+                refreshGUI(perfilInstagram);
+                dialog.dispose();
+            }
+        });
+        dialog.add(saveButton, BorderLayout.SOUTH);
+
+        dialog.setVisible(true);
+    }
+
+    public void updateAlbumList(PerfilInstagram perfilInstagram) {
+        albumPanel.removeAll(); // Clear the existing albums
+
+        for (Album album : perfilInstagram.getListaAlbumes()) {
+            JLabel albumLabel = new JLabel(
+                    album.toString());
+            albumLabel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+
+            JButton addPublicationsButton = new JButton("Agregar Publicaciones");
+            addPublicationsButton.addActionListener(e -> addPublicationsToAlbum(album,
+                    perfilInstagram));
+
+            JButton deleteButton = new JButton("Eliminar Álbum");
+            deleteButton.addActionListener(e -> {
+                perfilInstagram.removeAlbum(album);
+                updateAlbumList(perfilInstagram);
+            });
+
+            JPanel albumEntryPanel = new JPanel(new BorderLayout());
+            albumEntryPanel.add(albumLabel, BorderLayout.CENTER);
+            albumEntryPanel.add(addPublicationsButton, BorderLayout.WEST);
+            albumEntryPanel.add(deleteButton, BorderLayout.EAST);
+
+            albumPanel.add(albumEntryPanel);
+        }
+
+        albumPanel.revalidate(); // Update the albumPanel
+        albumPanel.repaint();
+    }
+
+    public void displayAlbum() {
+        SwingUtilities.invokeLater(() -> {
+            setVisible(true);
+            pack();
+        });
+    }
+}
